c69b85801ca388223ca6e15e9bb100617e588cb6
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-03-10 10:54:20 +0900

    UnityBasic1

---
+++ BasicStudy1/Assets/Scripts/ConditionExample.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ConditionExample : MonoBehaviour
+{
+    //연산자와 조건문
+    public int health = 100;
+
+    void Update()
+    {
+        health -= 1;
+        Debug.Log("Health : " + health);
+
+        //조건문
+        if(health <= 0)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
